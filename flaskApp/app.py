dec45b7ce58fede8a3f2919cc82acf63eb7d8f5c
Saito Takumi <saitoutkzzzzc@pm.nttdata.co.jp>
2019-03-01 15:07:25 +0900

    update flask_sample

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import numpy as np
+
 app = Flask(__name__)
 
+
+def picked_up():
+    """メッセージをランダムに表示するメソッド"""
+    messages = [
+        "こんにちは、あなたの名前を入力してください。",
+        "やあ！お名前は何ですか？",
+        "あなたの名前を教えてね"
+    ]
+    return np.random.choice(messages)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    title = 'ようこそ'
+    message = picked_up()
+    return render_template('index.html', message=message, title=title)
 
 
-@app.route('/hoge')
-def hoge():
-    return 'hoge!'
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ flaskApp/static/app.js
@@ -0,0 +1,5 @@
+(function () {
+    'user strict';
+
+    alert('アラート');
+})();
