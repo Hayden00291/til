696e19a46515571e5e8cc1efdd9d8c23c77cde24
Saito Takumi <saitoutkzzzzc@pm.nttdata.co.jp>
2019-03-13 17:16:51 +0900

    update ups.py

--- python/selenium/ups.py
+++ python/selenium/ups.py
@@ -31,41 +31,54 @@ options = ChromeOptions()
 driver = Chrome(executable_path='./chromedriver37.exe')
 driver.get('https://www.ups.com/mobile/ratetnthome?loc=ja_JP')
 
-
+# 発送元情報セット
+# 発送元国
 from_country_element = driver.find_element_by_id('orig_Country')
 from_country_selecter = Select(from_country_element)
 from_country_selecter.select_by_value(from_dict['country'])
-
+# 発送元郵便番号
 from_postalcode_element = driver.find_element_by_id('orig_PostalCode')
 from_postalcode_element.send_keys(from_dict['postal_code'])
-
+# 発送元都市
 from_city_element = driver.find_element_by_id('orig_City')
 from_city_element.send_keys(from_dict['city'])
 
-
+# 宛先情報セット
+# 宛先国
 to_country_element = driver.find_element_by_id('dest_Country')
 to_country_selecter = Select(to_country_element)
 to_country_selecter.select_by_value(to_dict['country'])
-
+# 宛先郵便番号
 to_postalcode_element = driver.find_element_by_id('dest_PostalCode')
 to_postalcode_element.send_keys(to_dict['postal_code'])
-
+# 宛先都市
 to_city_element = driver.find_element_by_id('dest_City')
 to_city_element.send_keys(to_dict['city'])
 
+# 貨物情報セット
+# 梱包タイプ
 package_type_element = driver.find_element_by_id('packageType')
 package_type_selecter = Select(package_type_element)
 package_type_selecter.select_by_value('02')
-
+# 重量
 package_weight = driver.find_element_by_id('packageWeight')
 package_weight.send_keys(package_dict['weight'])
-
+# 関税評価額
 customs_value = driver.find_element_by_id('customs-value')
 customs_value.send_keys(package_dict['total_price'])
 
+# スクレイピングお作法
 sleep(1)
 
+# 見積もり実行
 submit_btn = driver.find_element_by_id('quote_country_btn')
 submit_btn.click()
 
-# driver.quit()
+# 結果を取得、コンソールに表示
+results = driver.find_elements_by_class_name('list-group-item')
+
+for item in results:
+    print(item.text)
+    print('')
+
+driver.quit()
